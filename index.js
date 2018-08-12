3aa25cd856223ed6ee779d9cea72a39969031307
FaisalAli <faisalali1901@gmail.com>
2018-08-12 17:13:46 +0530

    added auth route and config passport

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const authRoutes = require('./routes/authRoutes');
+
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'bye' })
-})
+// Enable cookies
+app.use(cookieSession({
+	maxAge: 30 * 24 * 60 * 60 * 1000,
+	keys: [keys.cookieKey]
+}));
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+// Ask passport to use cookies
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) =>  done(null, user.id));
+
+passport.deserializeUser((id, done) => {
+	User.findOne({ _id: id }).then(user => done(null, user));
+});
+
+passport.use(new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback'
+	}, (accessToken, refreshToken, profile, done) => {
+		User.findOne({ googleId: profile.id })
+			.then(existingUser => {
+				if (existingUser) {
+					// We already have a user 
+					done(null, existingUser);
+				}else{
+					// We dont have a user create one
+					new User({ googleId: profile.id }).save()
+						.then(user => done(null, user));
+				}
+			});
+	})
+);
